753902e9dcb2f3bb3b723eb311ec490441a13457
Vadim <vadimpetrov8883@gmail.com>
2023-01-18 23:25:01 +0300

    create LoginPage

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LoginPageComponent} from "./core/page/login-page/login-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', component: LoginPageComponent }
+];
 
 @NgModule({
+  declarations: [LoginPageComponent],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
