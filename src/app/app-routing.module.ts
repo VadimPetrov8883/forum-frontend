a2fc773e11940510e41eb4ccdc0a5b4ca416f7da
Vadim Petrov <vadimpetrov8883@gmail.com>
2023-01-20 11:45:59 +0300

    add Material Angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {LoginPageComponent} from "./core/page/login-page/login-page.component";
+import {MaterialStyleModule} from "./module/MaterialStyle.module";
 
 const routes: Routes = [
   { path:'', component: LoginPageComponent }
@@ -8,7 +9,7 @@ const routes: Routes = [
 
 @NgModule({
   declarations: [LoginPageComponent],
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes), MaterialStyleModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
